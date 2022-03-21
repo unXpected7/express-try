c4c289e94684d1ba8894c659c89ec2fda19e8c06
unXpected7 <ilyasyusuf002@gmail.com>
2022-03-21 08:22:05 +0700

    change start script to default node app.js

--- app.js
+++ app.js
@@ -3,12 +3,4 @@ const app = express();
 const router = require('./routes');
 
 app.use(router);
-app.use((req, res, next) => {
-    res.status(404);
-    res.send({
-        status: failed,
-        message: ' Resources' + req.originalUrl + 'Not Found'
-    })
-})
-
 app.listen(3000, () => console.log("Server http://localhost:3000"))
